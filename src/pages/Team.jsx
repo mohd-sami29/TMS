4b9bdba9fac50916bd9965cdafcabef70a0983f4
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 16:18:18 +0530

    create Team component

---
+++ src/pages/Team.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function Team() {
+  return <Typography variant="h4">Welcome to Team Dashboard</Typography>;
+}
+
+export default Team;
