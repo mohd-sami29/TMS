7c70217d1a901984eb86606c2dd7dcedffbc6035
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 16:41:26 +0530

    add variant

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 const Home = () => {
-  return <Typography variant="h4">Welcome to Home</Typography>;
+  return <Typography variant="h4">Welcome to Home Dashboard</Typography>;
 };
 
 export default Home;
