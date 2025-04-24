71701eb21e46eafbeb3137461ec5c9a9de29ecd9
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 16:17:07 +0530

    create Master component

---
+++ src/pages/Master.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function Master() {
+  return (
+    <Typography variant='h4'>Welcome to Master Dashboard</Typography>
+  )
+}
+
+export default Master
