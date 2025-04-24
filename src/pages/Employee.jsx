e7f2820fcb1c8467c484c431f841c2e6702a3bef
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 11:07:45 +0530

    create new folder pages

---
+++ src/pages/Employee.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Employee = () => {
+  return <Typography variant="h4">Welcome to Employee Dashboard</Typography>;
+};
+
+export default Employee;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Home = () => {
+  return <Typography variant="h4">Welcome to Home</Typography>;
+};
+
+export default Home;
