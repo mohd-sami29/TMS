f6453d0fcb90abdd72c1142f2dd4e5b1bf5dc969
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 16:15:17 +0530

    create Alert component

---
+++ src/pages/Alert.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function Alert() {
+  return (
+    <Typography variant='h4'>Welcome to Alert Dashboard</Typography>
+  )
+}
+
+export default Alert
