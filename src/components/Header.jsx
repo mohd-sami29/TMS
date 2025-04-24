765142c8b4cb30ed49546d6de369e614232ef2e9
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-04-24 11:14:16 +0530

    create component folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Switch, Avatar, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Header = ({ darkMode, setDarkMode, toggleSidebar }) => {
+  const handleThemeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleSidebar}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My Dashboard
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <Switch checked={darkMode} onChange={handleThemeToggle} color="default" />
+          <Avatar alt="User" src="https://i.pravatar.cc/300" />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
