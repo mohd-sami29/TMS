3ae8ee5832f14fd6348a57891a194e5ea744fd08
MD SAMIULLAH <sami.mohd29@gmail.com>
2025-07-05 16:56:16 +0530

    UI updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Switch, Avatar, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
-const Header = ({ darkMode, setDarkMode, toggleSidebar }) => {
+const Header = ({ darkMode, setDarkMode, toggleSidebar, sidebarCollapsed }) => {
+  
   const handleThemeToggle = () => {
     setDarkMode(!darkMode);
   };
@@ -11,13 +13,13 @@ const Header = ({ darkMode, setDarkMode, toggleSidebar }) => {
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleSidebar}>
-          <MenuIcon />
+          {sidebarCollapsed?<MenuIcon />:<MenuOpenIcon />}
         </IconButton>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          My Dashboard
+          Admin Dashboard
         </Typography>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <Switch checked={darkMode} onChange={handleThemeToggle} color="default" />
+          <Switch id='switch' name='switch' checked={darkMode} onChange={handleThemeToggle} color="default" />
           <Avatar alt="User" src="https://i.pravatar.cc/300" />
         </Box>
       </Toolbar>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Root = () => {
+  const [mode, setMode] = React.useState('light');
+  const theme = createTheme({
+    palette: {
+      mode,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App setMode={setMode} mode={mode} />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
